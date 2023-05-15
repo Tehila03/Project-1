432ca61ec9fd1dd02cd69940eb8fea0d681ea257
tookey95 <petetookey@gmail.com>
2023-05-15 18:36:35 -0400

    weather functionality

--- assets/script.js
+++ assets/script.js
@@ -39,7 +39,44 @@ function clock(){
   timeInterval = setInterval(function() {
     now = dayjs().format("h:mm a")
     $("#timeWeather").text(now)
-    },1000);
+    },10000);
 }
 
 clock();
+
+var returnValue = {};
+var currentWeather = {};
+
+updateCoordinate(function (returnValue) {
+  console.log(returnValue);
+  var geoLocationLat=returnValue.latitude;
+      var geoLocationLon=returnValue.longitude;
+      var requestWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${geoLocationLat}&lon=${geoLocationLon}&exclude=hourly,daily&units=imperial&appid=71ab6dae80d4e4ec7fa98ea5618e8732`;
+      setWeatherIcon(requestWeatherUrl)
+    });
+
+function updateCoordinate(callback) {
+    navigator.geolocation.getCurrentPosition(
+      function (position) {
+        returnValue = {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude
+        }
+        callback(returnValue);
+      }
+    )
+}
+
+function setWeatherIcon(requestWeatherUrl){
+  fetch(requestWeatherUrl)
+      .then(function (response){
+        return response.json();
+      })
+      .then(function (data) {
+                var currentDayForecast_Icon = data.weather[0].icon;
+                var img = $('<img>',{ src: `https://openweathermap.org/img/wn/${currentDayForecast_Icon}@2x.png`})
+                $("#weatherIcon").prepend(img);
+            }
+      )};
+    
+    
